2fde46dfb2075ccd8a9cb764fdc540891c9a5d1e
narkisr <narkisr@gmail.com>
2013-01-16 19:50:07 +0200

    adding halt back

--- lib/middleware/network_binder.rb
+++ lib/middleware/network_binder.rb
@@ -26,16 +26,14 @@ module VagrantDns
   class Status
     # if socket is created on middleware initialization vagrant will get stuck
     def report(host,ip,status)
-	Thread.new {
-	  context = ZMQ::Context.new
-	  pub = context.socket(ZMQ::PUB)
-	  pub.connect URL
-	  UI.say(:debug,"connection made")
-	  res = pub.send("#{CHANNEL} #{host} #{ip} #{status.to_s}", ZMQ::NOBLOCK)
-	  UI.say(:info,"notifying dns server with #{status} status") if res
-	  UI.say(:info,"dns server isn't up, skiping notifying #{status} status") if not res
-	  pub.close
-	}.join
+	context = ZMQ::Context.new
+	pub = context.socket(ZMQ::PUB)
+	pub.connect URL
+	UI.say(:debug,"connection made")
+	res = pub.send("#{CHANNEL} #{host} #{ip} #{status.to_s}", ZMQ::NOBLOCK)
+	UI.say(:info,"notifying dns server with #{status} status") if res
+	UI.say(:info,"dns server isn't up, skiping notifying #{status} status") if not res
+	pub.close
     end
   end
 end
