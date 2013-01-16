e37cf3b6bf6197f5f97143b015e2d32f9a9ef9cf
narkisr <narkisr@gmail.com>
2013-01-16 19:31:44 +0200

    adding update to boot

--- lib/vagrant_init.rb
+++ lib/vagrant_init.rb
@@ -8,8 +8,8 @@ require 'zmq'
 
 #see http://tinyurl.com/bfuawuj  
 Vagrant.actions[:provision].insert_after(Vagrant::Action::VM::Provision, VagrantDns::Middleware::Update)
-Vagrant.actions[:start].insert_after(Vagrant::Action::VM::Provision, VagrantDns::Middleware::Update)
+Vagrant.actions[:start].insert_after(Vagrant::Action::VM::Boot, VagrantDns::Middleware::Update)
 
-# Vagrant.actions[:destroy].insert_after(Vagrant::Action::VM::ProvisionerCleanup, VagrantDns::Middleware::Remove)
+Vagrant.actions[:destroy].insert_after(Vagrant::Action::VM::ProvisionerCleanup, VagrantDns::Middleware::Remove)
 Vagrant.actions[:halt].insert_after(Vagrant::Action::VM::Halt, VagrantDns::Middleware::Remove)
 
