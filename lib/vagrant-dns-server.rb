3bc2a1650c120b438ff92028317dc45af0072afd
narkisr <narkisr@gmail.com>
2013-12-10 18:54:53 +0200

    adding forwardable and renaming plugin file to vagrant expected name

---
+++ lib/vagrant-dns-server.rb
@@ -0,0 +1,29 @@
+require 'vagrant'
+require 'zmq'
+# client
+require 'moneta'
+require 'vagrant_dns/consts'
+require 'vagrant_dns/configuration'
+require 'vagrant_dns/port'
+require 'vagrant_dns/middleware/update'
+require 'vagrant_dns/middleware/remove'
+require 'vagrant_dns/middleware/network_binder'
+
+#see http://tinyurl.com/bfuawuj  
+module VagrantDnsServer
+  class Plugin < Vagrant.plugin("2")
+    name 'vagrant-dns-server'
+    description <<-DESC
+    VagrantDnsServer
+    DESC
+
+
+    action_hook(:vagrant_dns_plugin) do |hook|
+	hook.after(::Vagrant::Action::Builtin::Provision, VagrantDns::Middleware::Update)
+	hook.after(VagrantPlugins::ProviderVirtualBox::Action::Boot, VagrantDns::Middleware::Update)
+    end
+
+  end
+end
+
+
--- lib/vagrant_dns/configuration.rb
+++ lib/vagrant_dns/configuration.rb
@@ -1,4 +1,4 @@
-
+require 'forwardable'
 
 module VagrantDns
   
